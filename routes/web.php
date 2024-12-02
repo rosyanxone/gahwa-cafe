7bb7bccbc131c64711812b9bd5a6b4637d61b75f
rosyanxone <rausyanfikrkarmayoga@gmail.com>
2024-12-02 16:53:57 +0800

    dashboard template

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::get('/dashboard', function () {
+    return view('pages.admin.dashboard');
+})->name('dashboard');
